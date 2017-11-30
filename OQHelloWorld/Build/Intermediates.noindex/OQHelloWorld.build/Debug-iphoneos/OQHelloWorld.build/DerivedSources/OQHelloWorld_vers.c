eaf955cdda5c2415ac4e7af65e56c4115f06f073
haze5959 <haze5959@naver.com>
2017-11-30 11:43:39 +0900

    project init

--- OQHelloWorld/Build/Intermediates.noindex/OQHelloWorld.build/Debug-iphoneos/OQHelloWorld.build/DerivedSources/OQHelloWorld_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char OQHelloWorldVersionString[];
- extern const double OQHelloWorldVersionNumber;
-
- const unsigned char OQHelloWorldVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:OQHelloWorld  PROJECT:OQHelloWorld-1" "\n";
- const double OQHelloWorldVersionNumber __attribute__ ((used)) = (double)1.;
